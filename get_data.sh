0ab11e5bd65533a39f9fd728d91936abfa824e65
Ubuntu <ubuntu@ip-172-31-25-240.us-east-2.compute.internal>
2021-10-03 20:08:09 +0000

    shell script for gettin g data

---
+++ get_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#wget http://vision.stanford.edu/yukezhu/visual7w_images.zip
+wget http://ai.stanford.edu/~yukez/papers/resources/dataset_v7w_telling.zip
+wget http://ai.stanford.edu/~yukez/papers/resources/dataset_v7w_pointing.zip
+wget http://ai.stanford.edu/~yukez/papers/resources/dataset_v7w_grounding_annotations.zip
